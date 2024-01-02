53083f0d2d473882ca9be5a86e61ef4c42ef4fc9
SteveImmanuel <steveandreasimanuel@gmail.com>
2024-01-03 07:47:30 +0900

    Use env var

--- configs.js
+++ configs.js
@@ -1,7 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 module.exports = {
-  SSH_TUNNEL_PORT: 6000,
-  TIMEOUT_DURATION: 60000,
-  USERNAME: 'admin',
-  PASSWORD: 'password',
-  JWT_SECRET: 'secret123'
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  JWT_SECRET: process.env.JWT_SECRET,
+  PORT: parseInt(process.env.PORT, 10),
+  TIMEOUT_DURATION: parseInt(process.env.TIMEOUT_DURATION, 10),
+  SSH_USERNAME: process.env.SSH_USERNAME,
+  SSH_IP: process.env.SSH_IP,
+  SSH_KEY_PATH: process.env.SSH_KEY_PATH,
+  DOWNLOAD_DIR: process.env.DOWNLOAD_DIR,
 }
