0b70cfae76751534c92b7776ec6bf68934344575
SteveImmanuel <steveandreasimanuel@gmail.com>
2024-01-03 14:14:47 +0900

    Implement home page to start job

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let data;
+    export let form;
+</script>
+
+<h1>Bypass Paywall</h1>
+<form method="POST">
+    <label>
+        Insert link:
+        <input
+            type="url"
+            placeholder="https://ieeexplore.ieee.org/..."
+            name="link"
+            autocomplete="off"
+            required
+        />
+    </label>
+    <button type="submit">Submit</button>
+</form>
+
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Link</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.jobs as job (job.id)}
+            <tr>
+                <td>{job.job_id}</td>
+                <td> <a href={job.link} target="_blank">{job.link}</a> </td>
+                {#if job.status === data.jobStatus.SUCCESS}
+                    <td> <a href='/files/{job.job_id}' target="_blank">DOWNLOAD</a> </td>
+                {:else if job.status === data.jobStatus.FAILED}
+                    <td>FAILED</td>
+                {:else if job.status === data.jobStatus.IN_PROGRESS}
+                    <td>IN_PROGRESS</td>
+                {/if}
+            </tr>
+        {/each}
+    </tbody>
+</table>
